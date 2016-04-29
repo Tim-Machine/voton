ebca68f6372dedbd24a879c9a62d8ad7bb1fed0a
Tim Smith <tim.smith@sparksgrove.com>
2016-04-29 08:58:33 -0400

    updates

--- client/createPoll/createPollHelper.js
+++ client/createPoll/createPollHelper.js
@@ -1,41 +1,40 @@
 Template.createPoll.events(
-{
-    "submit #createPoll": function(event, template)
     {
-        event.preventDefault();
-        var question = event.target.pollQuestion.value;
-        var token = window.localStorage.getItem('tempId');
+        "submit #createPoll": function (event, template) {
+            event.preventDefault();
+            var question = event.target.pollQuestion.value;
+            var token = window.localStorage.getItem('tempId');
 
-        Meteor.call("createPoll", question, token, function(error, result)
-        {
-            if (error)
-            {
-                console.log("error", error);
-            }
-            if (result)
-            {
-                var url = window.location.href;
-                newUrl = url.replace(location.search, "?poll=" + result);
-                window.location.href = newUrl;
-            }
-        });
-    },
-    'focus #pollQuestion': function(event, template)
-    {
-        var maxSize = '400px';
-        var currentWidth = $(event.target).width();
-        Session.set("currentWidth", currentWidth);
+            Meteor.call("createPoll", question, token, function (error, result) {
+                if (error) {
+                    console.log("error", error);
+                }
+                if (result) {
+                    if (location.search.length > 0) {
+                        var url = window.location.href;
+                        newUrl = url.replace(location.search, "?poll=" + result);
+                        window.location.href = newUrl;
+                    }
+                    else {
+                        window.location.href = window.location.href + "?poll=" + result;
+                    }
+                }
+            });
+        },
+        'focus #pollQuestion': function (event, template) {
+            var maxSize = '400px';
+            var currentWidth = $(event.target).width();
+            Session.set("currentWidth", currentWidth);
 
-        $(event.target).animate(
-        {
-            width: maxSize
-        }, 'fast');
-    },
-    'focusout #pollQuestion': function(event, template)
-    {
-        $(event.target).animate(
-        {
-            width: Session.get("currentWidth")
-        }, 'slow');
-    }
-});
+            $(event.target).animate(
+                {
+                    width: maxSize
+                }, 'fast');
+        },
+        'focusout #pollQuestion': function (event, template) {
+            $(event.target).animate(
+                {
+                    width: Session.get("currentWidth")
+                }, 'slow');
+        }
+    });
